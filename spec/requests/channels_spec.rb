97092e5cfea846d783e59ecaf59a69cf622e2681
Steve Goldman <steve.goldman@gmail.com>
2017-06-03 18:09:05 -0400

    Integrating authentication into request specs

--- spec/requests/channels_spec.rb
+++ spec/requests/channels_spec.rb
@@ -3,44 +3,59 @@ require "rails_helper"
 RSpec.describe "Channels API", type: :request do
 
   let!(:channels) { FactoryGirl.create_list(:channel, 10) }
+  let(:token) { Knock::AuthToken.new(payload: { sub: "user-token" }).token }
+  let(:headers) { { authorization: "Bearer #{token}" } }
 
   describe "GET /channels" do
-    before { get "/channels" }
-
-    it "returns channels" do
-      expect(json).to_not be_empty
+    it "does not allow an unauthenticated request" do
+      get "/channels"
+      assert_response :unauthorized
     end
 
-    it "returns status code 200" do
-      expect(response).to have_http_status(200)
-    end
-  end
+    context "when the request is authenticated" do
+      before { get "/channels", headers: headers }
 
-  describe "GET /channels/:id" do
-    context "when the record exists" do
-      before { get "/channels/#{channels.first.id}" }
-
-      it "returns the channel" do
+      it "returns channels" do
         expect(json).to_not be_empty
-        expect(json["id"]).to eq(channels.first.id)
       end
 
       it "returns status code 200" do
         expect(response).to have_http_status(200)
       end
     end
-    
-    context "when the record does not exist" do
-      before { get "/channels/99999" }
-      
-      it "returns status code 404" do
-        expect(response).to have_http_status(404)
+  end
+
+  describe "GET /channels/:id" do
+    it "does not allow an unauthenticated request" do
+      get "/channels/#{channels.first.id}"
+      assert_response :unauthorized
+    end
+
+    context "when the request is authenticated" do
+      context "when the record exists" do
+        before { get "/channels/#{channels.first.id}", headers: headers }
+
+        it "returns the channel" do
+          expect(json).to_not be_empty
+          expect(json["id"]).to eq(channels.first.id)
+        end
+
+        it "returns status code 200" do
+          expect(response).to have_http_status(200)
+        end
       end
 
-      it "returns a not found message" do
-        expect(response.body).to match(/Couldn't find Channel/)
+      context "when the record does not exist" do
+        before { get "/channels/99999", headers: headers }
+
+        it "returns status code 404" do
+          expect(response).to have_http_status(404)
+        end
+
+        it "returns a not found message" do
+          expect(response.body).to match(/Couldn't find Channel/)
+        end
       end
     end
   end
-  
 end
--- spec/requests/items_spec.rb
+++ spec/requests/items_spec.rb
@@ -4,57 +4,72 @@ RSpec.describe "Items API" do
 
   let!(:channel) { create(:channel) }
   let!(:items) { FactoryGirl.create_list(:item, 20, channel: channel) }
+  let(:token) { Knock::AuthToken.new(payload: { sub: "user-token" }).token }
+  let(:headers) { { authorization: "Bearer #{token}" } }
 
   describe "GET /channels/:channel_id/items" do
-    context "when channel exists" do
-      before { get "/channels/#{channel.id}/items" }
-
-      it "returns status code 200" do
-        expect(response).to have_http_status(200)
-      end
-
-      it "returns all todo items" do
-        expect(json.size).to eq(items.count)
-      end
+    it "does not allow an unauthenticated request" do
+      get "/channels/#{channel.id}/items"
+      assert_response :unauthorized
     end
 
-    context "when channel does not exist" do
-      before { get "/channels/999999/items" }
+    context "when the request is authenticated" do
+      context "when channel exists" do
+        before { get "/channels/#{channel.id}/items", headers: headers }
 
-      it "returns status code 404" do
-        expect(response).to have_http_status(404)
+        it "returns status code 200" do
+          expect(response).to have_http_status(200)
+        end
+
+        it "returns all todo items" do
+          expect(json.size).to eq(items.count)
+        end
       end
 
-      it "returns a not found message" do
-        expect(response.body).to match(/Couldn't find Channel/)
+      context "when channel does not exist" do
+        before { get "/channels/999999/items", headers: headers }
+
+        it "returns status code 404" do
+          expect(response).to have_http_status(404)
+        end
+
+        it "returns a not found message" do
+          expect(response.body).to match(/Couldn't find Channel/)
+        end
       end
     end
   end
 
   describe "GET /channels/:channel_id/items/:id" do
-    context "when item exists" do
-      before { get "/channels/#{channel.id}/items/#{items.first.id}" }
-
-      it "returns status code 200" do
-        expect(response).to have_http_status(200)
-      end
-
-      it "returns the item" do
-        expect(json["id"]).to eq(items.first.id)
-      end
+    it "does not allow an unauthenticated request" do
+      get "/channels/#{channel.id}/items/#{items.first.id}"
+      assert_response :unauthorized
     end
 
-    context "when item does not exist" do
-      before { get "/channels/#{channel.id}/items/999999" }
+    context "when the request is authenticated" do
+      context "when item exists" do
+        before { get "/channels/#{channel.id}/items/#{items.first.id}", headers: headers }
 
-      it "returns status code 404" do
-        expect(response).to have_http_status(404)
+        it "returns status code 200" do
+          expect(response).to have_http_status(200)
+        end
+
+        it "returns the item" do
+          expect(json["id"]).to eq(items.first.id)
+        end
       end
 
-      it "returns a not found message" do
-        expect(response.body).to match(/Couldn't find Item/)
+      context "when item does not exist" do
+        before { get "/channels/#{channel.id}/items/999999", headers: headers }
+
+        it "returns status code 404" do
+          expect(response).to have_http_status(404)
+        end
+
+        it "returns a not found message" do
+          expect(response.body).to match(/Couldn't find Item/)
+        end
       end
     end
   end
-  
 end
